042dc3f9b0ecbc751ddb3e95061d0c3cdf409c1d
Sriragavi M <sriragaviiimohan@gmail.com>
2025-09-28 19:05:55 +0530

    Sri

--- question13.sh
+++ question13.sh
@@ -1,7 +1,10 @@
-git checkout -b newbranch
-git rm *.sh
+git checkout -b branch13
+rm *.sh
+git add -u
+git commit -m "Ques 13"
 touch file13.txt
 git add file13.txt
 git commit -m "Question 13 Sriragavi M"
-git push origin newbranch
+git push -u origin branch13
+
 
